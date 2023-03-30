a7d11ad79f69321fab69141bec7d67461b444edb
AlexanderKara <karabchuk@gmail.com>
2023-03-30 13:18:07 +0300

    Конфиг для netlify

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const path = require("path")
 
 const port = process.env.PORT || 3000;
 const app = express();
-const pathstr = path.join(__dirname, "../dist/index.html")
+const pathStr = path.join(__dirname, "../dist/index.html")
 
 app.use(express.static("./dist"))
 app.get("/*", (req, res) => {
-    res.sendFile(pathstr)
+    res.sendFile(pathStr)
 })
 console.log(port);
 app.listen(port);
